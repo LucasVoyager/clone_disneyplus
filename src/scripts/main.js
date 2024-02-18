5dd98fd3dc6e1d942e6c1257bb36918510c9f4c8
Lucas barros <fallenagamesytb@gmail.com>
2024-02-18 20:57:23 -0300

    feat: section faq and make toggle using js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     for (let i=0; i< buttons.length; i++) {
         buttons[i].addEventListener('click', function(butt) {
@@ -11,7 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
             butt.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openAndCloseQuestion)
+    }
 })
+
+function openAndCloseQuestion(element) {
+    const classe = 'faq__questions__item--is-open';
+    console.log(element);
+    const elemDad = element.target.parentNode;
+
+    elemDad.classList.toggle(classe);
+}
 
 function hideTabs() {
     const tabContainers = document.querySelectorAll('[data-tab-id]');
